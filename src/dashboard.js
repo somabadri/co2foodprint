3b3a8c238da67187e685f66f48b21141dd8e8130
Soma Sai Kumar Badri <sobadri@ucsc.edu>
2021-04-29 11:01:33 -0700

    added dashboard

---
+++ src/dashboard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import '../styles/dashboard.scss'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+          <div className="left-section">
+              <div className="tracker">
+
+              </div>
+              <div className="buttons">
+                
+              </div>
+          </div>
+          <div className="right-section">
+              <div className="post"></div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;

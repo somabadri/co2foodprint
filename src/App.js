e34c6f5deafabf91cb9a840430ec666862acc2a3
Soma Sai Kumar Badri <sobadri@ucsc.edu>
2021-04-28 17:39:56 -0700

    landing

--- src/App.js
+++ src/App.js
@@ -1,75 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Navbar from './components/navbar';
-import LoginHooks from './components/LoginHooks';
-import LogoutHooks from './components/LogoutHooks';
-import GoogleBtn from './GoogleBtn';
+import Landing from './landing';
 
 
 function App() {
   return (
-    <Router>
       <div>
-        <Navbar />
-        <LoginHooks />
-        <LogoutHooks />
-        <GoogleBtn/>
-
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/calculate'>Calculate</Link>
-          </li>
-          <li>
-            <Link to='/userProfile'>UserProfile</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/"><Home/></Route>
-          <Route path="/about"><About/></Route>
-          <Route path="/calculate"><Calculate/></Route>
-          <Route path="/userProfile"><UserProfile/></Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function About() {
-  return (
-      <div>
-          This is the about page
+        <Landing />
       </div>
   );
 }
-
-function Home() {
-  return (
-      <div>
-          this is the home page
-      </div>
-  );
-}
-
-function Calculate() {
-  return (
-      <div>
-          This is the Calculate page
-      </div>
-  );
-}
-
-function UserProfile() {
-  return (
-      <div>
-          This is the user profile page
-      </div>
-  );
-}
-
 export default App;
--- src/landing.js
+++ src/landing.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Navbar from './components/navbar';
+import './styles/landing.scss';
+import monstera from './assets/monstera.png';
+import Googlebtn from './components/GoogleBtn'
 
 
 function Landing() {
   return (
     <div>
+      <Navbar />
+      <div className="container">
+        <div className= "left-text">
+          <div className= "title">
+            Carbon Foodprint
+          </div>
+          <div className="subtitle"> 
+            A tool that helps you lead the sustainable life of your choice. 
+          </div> 
+          <div className="description">
+            Keep track of your carbon emissions, set weekly goals, share your enviroment friendly recipes, 
+            and watch yourself and your friends hit their goals.
+          </div>
+          <Googlebtn />
+        </div>
+        <div className= "right-text">
+          <img src={monstera} alt='monstera plant'/>
+        </div>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }

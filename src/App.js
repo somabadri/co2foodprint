ac9d61253b0fa11c547403ec32b5c6523373ec8d
kcbroder <kcbroder@ucsc.edu>
2021-04-29 18:20:34 -0700

    ugly calculate food page dont use this yet

--- src/App.js
+++ src/App.js
@@ -1,99 +1,108 @@
 import React from 'react';
-import './style.scss';
+import './styleCalcFood.scss';
 
-  function App() {
-    return <Profile {...profileData} />;
-  }
-  
-  export default App;
-  
-  function Profile(props) {
-    const {
-      pngtreenordicPresentDecorationGreen,
-      dashboard,
-      yourRecipes,
-      yourMetrics,
-      ellipse9,
-      sobadri,
-      text1,
-      rectangle21,
-      line1,
-      line2,
-      line3,
-      line4,
-      friends,
-      ellipse10,
-      ellipse11,
-      ellipse12,
-      ellipse13,
-      carbonCutters2021,
-    } = props;
-  
-    return (
-      <div class="container-center-horizontal">
-        <div className="profile screen">
-          <div className="flex-col-2">
-            <div className="overlap-group1">
-              <img className="pngtreenordic-p-plant3799519-2" src={pngtreenordicPresentDecorationGreen} />
-              <div className="dashboard taviraj-normal-tuna-20px">{dashboard}</div>
-            </div>
-            <div className="flex-row">
-              <h1 className="title lato-bold-black-25px">{yourRecipes}</h1>
-              <div className="your-metrics lato-bold-black-25px">{yourMetrics}</div>
-            </div>
+function App() {
+  return <CalculateFood {...calculateFoodData} />;
+}
+
+export default App;
+
+function CalculateFood(props) {
+  const {
+    pngtreenordicPresentDecorationGreen,
+    profile,
+    text16,
+    baseline_Fastfood_White_48Dp1,
+    baseline_Directions_Car_White_48Dp1,
+    food,
+    transportation,
+    quantity,
+    item,
+    text17,
+    submit,
+    cancel,
+    overlapGroup4,
+    carbonCutters2021,
+  } = props;
+
+  return (
+    <div class="container-center-horizontal">
+      <div className="calculate-food screen">
+        <div className="overlap-group2">
+          <img className="pngtreenordic-p-plant3799519-2" src={pngtreenordicPresentDecorationGreen} />
+          <div className="profile taviraj-normal-tuna-20px">{profile}</div>
+        </div>
+        <h1 className="text-1 lato-bold-black-30px">{text16}</h1>
+        <div className="flex-row-4">
+          <div className="overlap-group">
+            <img className="baseline" src={baseline_Fastfood_White_48Dp1} />
           </div>
-          <div className="flex-row-2">
-            <div className="flex-row-3">
-              <div className="flex-col">
-                <img className="ellipse-9" src={ellipse9} />
-                <div className="sobadri">{sobadri}</div>
-              </div>
-              <div className="text-1 lato-normal-black-20px">{text1}</div>
-            </div>
-            <div className="flex-col-1">
-              <div className="overlap-group">
-                <img className="rectangle-21" src={rectangle21} />
-                <img className="line-1" src={line1} />
-                <img className="line-2" src={line2} />
-                <img className="line-3" src={line3} />
-                <img className="line-4" src={line4} />
-              </div>
-              <div className="friends lato-bold-black-25px">{friends}</div>
-              <div className="flex-row-1">
-                <img className="ellipse-10" src={ellipse10} />
-                <img className="ellipse-1" src={ellipse11} />
-                <img className="ellipse-1" src={ellipse12} />
-                <img className="ellipse-1" src={ellipse13} />
-              </div>
-            </div>
-          </div>
-          <div className="overlap-group2">
-            <div className="carbon-cutters-2021 taviraj-normal-tuna-20px">{carbonCutters2021}</div>
+          <div className="overlap-group3">
+            <img className="baseline" src={baseline_Directions_Car_White_48Dp1} />
           </div>
         </div>
+        <div className="flex-row">
+          <div className="food lato-bold-black-25px">{food}</div>
+          <div className="transportation">{transportation}</div>
+        </div>
+        <div className="flex-row-2">
+          <div className="quantity lato-bold-black-30px">{quantity}</div>
+          <div className="item lato-bold-black-30px">{item}</div>
+        </div>
+        <div className="flex-row-5">
+          <div className="rectangle-1 border-1px-black"></div>
+          <div className="rectangle-1-1 border-1px-tuna"></div>
+        </div>
+        <div className="flex-row-1">
+          <div className="rectangle-1 border-1px-black"></div>
+          <div className="rectangle-1-1 border-1px-tuna"></div>
+        </div>
+        <div className="flex-row-1">
+          <div className="rectangle-1 border-1px-tuna"></div>
+          <div className="rectangle-1-1 border-1px-tuna"></div>
+        </div>
+        <div className="flex-row-1">
+          <div className="rectangle-1 border-1px-tuna"></div>
+          <div className="rectangle-1-1 border-1px-tuna"></div>
+        </div>
+        <div className="flex-row-3">
+          <div className="rectangle-1 border-1px-tuna"></div>
+          <div className="rectangle-19 border-1px-tuna"></div>
+        </div>
+        <div className="overlap-group1">
+          <div className="rectangle-3"></div>
+          <div className="text-2 taviraj-normal-white-30px">{text17}</div>
+        </div>
+        <div className="flex-row-6">
+          <div className="overlap-group5">
+            <div className="submit lato-bold-white-30px">{submit}</div>
+          </div>
+          <div className="overlap-group6">
+            <div className="cancel lato-bold-white-30px">{cancel}</div>
+          </div>
+        </div>
+        <div className="overlap-group4" style={{ backgroundImage: `url(${overlapGroup4})` }}>
+          <div className="carbon-cutters-2021 taviraj-normal-tuna-20px">{carbonCutters2021}</div>
+        </div>
       </div>
-    );
-  }
-  
-  const profileData = {
-      pngtreenordicPresentDecorationGreen: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/-pngtree-nordic-present-decoration-green-plant-3799519-2@2x.png",
-      dashboard: "Dashboard",
-      yourRecipes: "Your Recipes",
-      yourMetrics: "Your Metrics",
-      ellipse9: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/ellipse-9@2x.png",
-      sobadri: "sobadri",
-      text1: <>Recipe 1<br />Recipe 2<br />Recipe 3<br />Recipe 4<br />Recipe 5<br />Recipe 1<br />Recipe 2<br />Recipe 3<br />Recipe 4<br />Recipe 5<br />Recipe 1<br />Recipe 2<br />Recipe 3<br />Recipe 4<br />Recipe 5</>,
-      rectangle21: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/rectangle-21@2x.svg",
-      line1: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/line-1@2x.svg",
-      line2: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/line-2@2x.svg",
-      line3: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/line-1@2x.svg",
-      line4: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/line-4@2x.svg",
-      friends: "Friends",
-      ellipse10: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/ellipse-10@2x.png",
-      ellipse11: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/ellipse-10@2x.png",
-      ellipse12: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/ellipse-10@2x.png",
-      ellipse13: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/ellipse-10@2x.png",
-      carbonCutters2021: "Carbon Cutters 2021",
-  };
-  
-  
+    </div>
+  );
+}
+
+const calculateFoodData = {
+    pngtreenordicPresentDecorationGreen: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/-pngtree-nordic-present-decoration-green-plant-3799519-2@2x.png",
+    profile: "Profile",
+    text16: "Choose Entry Type Below",
+    baseline_Fastfood_White_48Dp1: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/baseline-fastfood-white-48dp-1@2x.png",
+    baseline_Directions_Car_White_48Dp1: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/baseline-directions-car-white-48dp-1@2x.png",
+    food: "Food",
+    transportation: "Transportation",
+    quantity: "Quantity",
+    item: "Item",
+    text17: "+",
+    submit: "Submit",
+    cancel: "Cancel",
+    overlapGroup4: "https://anima-uploads.s3.amazonaws.com/projects/608b4ca9ee3fce15866ca79a/releases/608b51b1f68e88411d270394/img/rectangle-2@1x.svg",
+    carbonCutters2021: "Carbon Cutters 2021",
+};
+

06028b20440792df60b31b64920c3a7aae4975ef
kcbroder <kcbroder@ucsc.edu>
2021-05-07 15:50:33 -0700

    transport page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, withRouter  } from 'react-router-dom';
 import Profile from './pages/Profile'
-import CalculateFood from './pages/CalcFood';
+import CalculateTransport from './pages/CalcTransport';
 import PostRecipe from './pages/PostRecipe';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
@@ -15,7 +15,7 @@ function App() {
           <Route exact path="/" component={withRouter(Landing)}/>
           <Route path="/dashboard/" component={withRouter(Dashboard)}/>
           <Route path="/about/" component={withRouter(About)}/>
-          <Route path="/calculate/" component={withRouter(CalculateFood)}/>
+          <Route path="/calculate/" component={withRouter(CalculateTransport)}/>
           <Route path="/postrecipe/" component={withRouter(PostRecipe)}/>
           <Route path="/profile/" component={withRouter(Profile)}/>
         </Switch>

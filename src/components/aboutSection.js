2dad76d25ae2ec8176a72cb9e5c362d8bf31304e
Soma Sai Kumar Badri <sobadri@ucsc.edu>
2021-05-15 17:09:43 -0700

    fixed about and styling for footer

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import '../styles/aboutSection.scss'
-import profile from '../assets/profile.jpg';
 
 class AboutSection extends Component {
   constructor(props) {
     super(props);
-    this.state = {description: '' };
+    this.state = {description: '',
+                  profile:''};
   }
 
     render() {
       return (
         <div className='about'>
-          <img className='profile-picture' src={profile} alt='profile' />
+          <img className='profile-picture' src= {this.props.profile} alt='profile' />
           <div className='about-text'>
            {this.props.description}
           </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,19 +3,28 @@ import AboutSection from "../components/AboutSection";
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import '../styles/about.scss';
+import destinpic from '../assets/destin.jpg';
+import somapic from '../assets/soma.jpg';
+import kelleypic from '../assets/kelley.jpg';
+import raymondpic from '../assets/raymond.jpg';
+import rileypic from '../assets/riley.jpg';
 
 class About extends Component {
     render() {
-        let description= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non diam sed sem suscipit mollis mattis eu quam. Morbi luctus massa tellus, quis dignissim lectus condimentum at. Nullam convallis non tellus in gravida. ';
+        let soma= 'My name is Soma Badri, I am a graduating computer science student at UC Santa Cruz. I am intersted in sustainable living, specifically limiting my carbon footprint. This app was an oppurtunity to create a tool to aid that goal. ';
+        let riley='My name is Riley Murphy, I am a 4th year computer science student at UC Santa Cruz. I wanted to make this application because of the issues regarding carbon emissions that negatively impact our planet. I wanted to create an app that allows people to work to reduce their carbon emission rates and better improve the world.';
+        let kelley='I am Kelley, a 3rd year cs & math major. I wanted to help create this app bc I am interested in eco-conservation stuff and thought it would be useful. Fun fact I like cats I am on a mission to pet every cat in my neighborhood';
+        let raymond='I am Raymond, a 3rd year CS major, wanted to create this app because I wanted to personally track my own carbon emissions.';
+        let destin ='I am Destin, a third year Computer Science major at UCSC. I like walking my two dogs Oreo and Charlie, and ice skating.';
         return (
         <div>
             <Navbar />
             <div className="posts-container">
-                <AboutSection description={description} />
-                <AboutSection description={description} />
-                <AboutSection description={description} />
-                <AboutSection description={description} />
-                <AboutSection description={description} />
+                <AboutSection profile= {somapic} description={soma} />
+                <AboutSection profile= {rileypic} description={riley} />
+                <AboutSection profile= {kelleypic} description={kelley} />
+                <AboutSection profile= {raymondpic} description={raymond} />
+                <AboutSection profile= {destinpic} description={destin} />
             </div>
             <Footer />
         </div>

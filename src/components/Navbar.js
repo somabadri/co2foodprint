bae43a019be14a6c0a477fb457e28d90c5a7a395
Raymond Lee <74083400+Raymond-YZ@users.noreply.github.com>
2021-05-27 11:13:44 -0700

    touched up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,62 @@
 import React from 'react';
-import '../styles/navbar.css';
-import {useState,useEffect} from 'react';
-import { Navbar, Nav, NavDropdown} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+//import '../styles/navbar.css'
 import GoogleBtn from './GoogleBtn';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function NavbarComponent() {
-  const [name,setName] = useState('');
-  const [pic,setPic] = useState('');
 
-  useEffect(()=> {
-    const data = {
-      name: localStorage.getItem('current name'),
-      email: localStorage.getItem('current email'),
-      pic: localStorage.getItem('current pic')
-    }
-    setName(data.name);
-    setPic(data.pic);
-  },[name]);
+class NavbarComponent extends React.Component {
+  state = { clicked: false }
 
-  return (  
-    <Navbar collapseOnSelect expand="md" style={{
-      backgroundColor: "#84A98C", fontFamily: "Taviraj", height: "60px",
-    }}>
-      <Navbar.Brand>CO2 Foodprint</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="/about">About us</Nav.Link>
-          <NavDropdown title="Calculate" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/calculate">Food</NavDropdown.Item>
-            <NavDropdown.Item href="/calculateTransport">Transportation</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        {
-          name ?
-            <React.Fragment>
-              <Nav>
-                <Nav.Link href="/profile">Hi {name}!</Nav.Link>
-              </Nav>
-              <Navbar.Brand href="/profile">
-                <img src={pic} width="50px" height="50px" style={{marginTop: 3}} alt=''/>
-              </Navbar.Brand>
-              
-            </React.Fragment>
-            : <div></div>
-        }
-        <GoogleBtn />
-      </Navbar.Collapse>
-    </Navbar>
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    let name = localStorage.getItem('firstname');
+    let profileImg = localStorage.getItem('profilepic');
+    console.log(profileImg);
+    return (
+
+      <Navbar collapseOnSelect expand="lg" style={{
+        backgroundColor: "#84A98C", fontFamily: "Taviraj", fontSize: 16
+      }}>
+        <Navbar.Brand href="#" style={{ fontSize: 24 }}>CO2 Foodprint</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <NavDropdown title="Calculate" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/calculate">Food</NavDropdown.Item>
+              <NavDropdown.Item href="/calculateTransport">Transportation</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          {
+            name ?
+              <React.Fragment>
+                <Navbar.Brand href="/profile">
+                  <img src={profileImg} width="50px" height="50px" alt="profilepic"/>
+                </Navbar.Brand>
+                <Nav>
+                  <Nav.Link href="/profile" style={{ fontWeight: "bold" }}>Hi {name}!</Nav.Link>
+                </Nav>
+              </React.Fragment>
+              : <div></div>
+          }
+          <Nav>
+            <Nav.Link href="/about">About Us</Nav.Link>
+          </Nav>
+          <GoogleBtn />
+          
+        </Navbar.Collapse>
+
+      </Navbar>
     );
+
+
+  };
 }
 
 export default NavbarComponent;
